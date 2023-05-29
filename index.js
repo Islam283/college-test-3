27031fe41383fa93cd2b5f021ffcde9f3675a687
islamix <27islamix@gmail.com>
2023-05-29 23:21:44 +0300

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ export default function solution(content) {
   const rows = content.split('\n').slice(2)
   console.log(`Всего растений: ${rows.length}`)
   const data = rows.map(row=>row.split('|').filter(row=>row)).map(row => row.map(element => element.trim()))
+
   const names =data.map(row => row[0])
   const sortedNames = names.map((name) => name[0].toUpperCase() + name.slice(1)).sort()
   console.log(sortedNames)
@@ -15,11 +16,16 @@ export default function solution(content) {
   const forestPlants = data.filter(row => row[1].split(',')[0] ==='Леса')
   const calcAverage = (years) => {
     if (years.includes('-')) {
-      return (Number(years.split('-')[0]) + Number(years.split('-')[1].split('')[0])) /2
+      return (Number(years.split('-')[0]) + Number(years.split('-')[1].split(' ')[0])) /2
     } return Number(years.split(' ')[0])
   }
   const forestYears = forestPlants.map(row => calcAverage(row[3]))
   const ageTotal = forestYears.reduce((acc, elem) => acc + elem)
-  console.log(`Средний возраст для всех лесных растений: ${Math.round(ageTotal / forestYears.length)} лет`)
+  
+   console.log(`Средний возраст для всех лесных растений: ${Math.round(ageTotal / forestYears.length)} лет`)
+
+   const dangerousOnlyRows = data.filter(row => row[4] ==='Да')
+   const dangerousHabitatas = dangerousOnlyRows.map((row) => row[1].split(', ').map(place => place[0].toUpperCase() + place.slice(1)))
+   console.log(dangerousHabitatas)
   // END
-}
+} 
